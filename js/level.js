c4bcd0975ce85ce5b40a8ee3080b493f11404f1b
Hannah Mitchell <hannahmitchie@yahoo.co.uk>
2014-05-18 21:11:12 +0100

    added code comments of understanding to level.js

--- js/level.js
+++ js/level.js
@@ -1,4 +1,4 @@
-//This defines what aliens are drawn and where abouts on the board
+//This defines what aliens are drawn and where abouts they are drawn on the board
 
   var levelData = { 
      1:  [[0,0,0,0,0,0,0,0,0,0,0],
@@ -24,12 +24,18 @@
           [0,0,1,1,1,1,1,1,1,1,0],
           [0,0,1,1,1,1,1,1,1,1,0]] };
 
+// This is data about the sprite image. 
+//The class of the image is defined for what javascript can refer to, the sx is the horiziontal position on the photoshop document, the sy is the y position (vertical), w is ther width of the image size and h is the height of the sprite. 
+//cls is the class of the alien in html so it can be identified and the frames is how many animation stages the sprite has until back to the beginning again.
+
   var spriteData = {
     'alien1': { sx: 0,  sy: 0,  w: 23, h: 18, cls: Alien, frames: 2 },
     'alien2': { sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 2 },
     'player': { sx: 0,  sy: 36, w: 26, h: 17, cls: Player },
     'missile': { sx: 0,  sy: 86, w: 3,  h: 14, cls: Missile }
   }
+  
+  //this function looks to loads a new start game, and prompts the user to use the action of the spacebar 
 
   function startGame() {
     var screen = new GameScreen("Alien Invaders","press space to start",
@@ -40,6 +46,8 @@
     Game.loop();
   }
 
+// This function looks to end the game, display some text for the user and if they user presses space, then Gameboard 1 (Level 1?) loads
+
   function endGame() {
     var screen = new GameScreen("Game Over","(press space to restart)",
                                  function() {
@@ -48,6 +56,7 @@
     Game.loadBoard(screen);
   }
 
+// This function seems to load when the user has won the gamneand got through a certaina mount of levels
 
   function winGame() {
     var screen = new GameScreen("You Win!","(press space to restart)",
@@ -57,6 +66,7 @@
     Game.loadBoard(screen);
   }
 
+//something to do with audio, not sure how it works
   $(function() {
     GameAudio.load({ 'fire' : 'media/laser.ogg', 'die' : 'media/explosion.ogg' }, 
                    function() { 
